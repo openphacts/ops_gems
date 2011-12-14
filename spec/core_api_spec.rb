d429170d027e92528b1e636d1d91f368036b1929
Thomas Marek <thomas.marek@biosolveit.de>
2011-12-14 17:47:27 +0100

    Add more tests

--- spec/core_api_spec.rb
+++ spec/core_api_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe OPS::CoreApiCall do
-  it "calls the OPS core API and parses the result" do
-    stubbed_request = stub_request(:post, "http://ops.few.vu.nl:9183/opsapi").
-         with(:body => {"method"=>"compoundLookup", "substring"=>"Sora", "limit"=>"2", "offset"=>"0"},
-              :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'}).
-         to_return(%{HTTP/1.1 200 OK
+  before(:all) do
+    @http_response_compound_lookup = %{HTTP/1.1 200 OK
 Date: Tue, 06 Dec 2011 11:21:25 GMT
 Accept-Ranges: bytes
 Server: Restlet-Framework/2.0.0
@@ -37,17 +34,63 @@ Content-Type: application/sparql-results+xml; charset=UTF-8
       </binding>
     </result>
   </results>
-</sparql>})
+</sparql>}
+  end
 
+  def stub_core_api_request(method, options, url="http://ops.few.vu.nl:9183/opsapi")
+    stringified_options = {}
+    options.each { |key, value| stringified_options[key.to_s] = value.to_s }
+
+    stringified_options["method"] = method
+
+    stub_request(:post, url).
+      with(:body => stringified_options,
+            :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'})
+  end
+
+  it "calls the OPS core API with all specified options and parses the result" do
     core_api_call = OPS::CoreApiCall.new
-    options = Hash.new
-    options[:substring] = "Sora"
-    options[:limit] = 2
+    options = {
+      :substring => "Sora",
+      :limit => 2,
+      :offset => 3
+    }
+
+    stubbed_request = stub_core_api_request("compoundLookup", options).to_return(@http_response_compound_lookup)
+
     results = core_api_call.request("compoundLookup", options)
 
     stubbed_request.should have_been_made.once
     results.should == [
       {:compound_uri=>"http://rdf.chemspider.com/187440", :compound_name=>"Sorafenib"},
       {:compound_uri=>"http://rdf.chemspider.com/3971", :compound_name=>"8-Methoxypsoralen"}]
+  end
+
+  it "should set the offset to 0 by default" do
+    core_api_call = OPS::CoreApiCall.new("http://ops.few.vu.nl:9183/opsapi")
+    options = {
+      :substring => "Sora",
+      :limit => 100
+    }
+
+    stubbed_request = stub_core_api_request("compoundLookup", options.merge({:offset => 0})).to_return(@http_response_compound_lookup)
+
+    core_api_call.request("compoundLookup", options)
+
+    stubbed_request.should have_been_made.once
+  end
+
+  it "should set the limit to 100 by default" do
+    core_api_call = OPS::CoreApiCall.new("http://ops.few.vu.nl:9183/opsapi")
+    options = {
+      :substring => "Sora",
+      :offset => 0
+    }
+
+    stubbed_request = stub_core_api_request("compoundLookup", options.merge({:limit => 100})).to_return(@http_response_compound_lookup)
+
+    core_api_call.request("compoundLookup", options)
+
+    stubbed_request.should have_been_made.once
   end
 end
