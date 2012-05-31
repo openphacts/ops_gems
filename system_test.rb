90dca0ea679b89c46408ef22eb826e4cb0ba5f97
Thomas Marek <thomas.marek@biosolveit.de>
2012-05-31 15:02:26 +0200

    Improve the system test

--- system_test.rb
+++ system_test.rb
@@ -9,9 +9,14 @@ OPS_URL = "http://ops.few.vu.nl:9187/opsapi"
 CHEMSPIDER_TOKEN = ""
 
 
+raise "No OPS Url defined" if OPS_URL.empty?
+raise "No ChemSpider Token defined" if CHEMSPIDER_TOKEN.empty?
+
+
 def make_core_api_call(method, options)
   OPS::CoreApiCall.new(OPS_URL).request(method, options)
 end
+
 
 
 make_core_api_call("compoundLookup",
@@ -27,7 +32,7 @@ make_core_api_call("compoundPharmacology",
 make_core_api_call("proteinPharmacology",
                    :uri => "<http://chem2bio2rdf.org/chembl/resource/chembl_targets/12261>")
 make_core_api_call("proteinPharmacology",
-                   :uri => "<http://chem2bio2rdf.org/chembl/resource/chembl_targets/12261>")
+                   :uri => "<http://www.conceptwiki.org/concept/458eaa59-79a5-448f-9085-9664f6f643af>")
 make_core_api_call("chemspiderInfo",
                    :csids => "1,3")
 make_core_api_call("subclasses",
@@ -61,9 +66,17 @@ make_core_api_call("objects",
                    :predicate => "<http://brenda-enzymes.info/has_ic50_value_of>")
 
 
-make_core_api_call("chemicalSubstructureSearch",
-                   :smiles => "CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl",
-                   :chemspider_token => CHEMSPIDER_TOKEN)
 make_core_api_call("chemicalExactStructureSearch",
-                   :smiles => "CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl",
+                   :smiles => %(O=C3C(/Oc1ccccc1)=C(/c2ccc(cc2)S(=O)(=O)C)CC3),
                    :chemspider_token => CHEMSPIDER_TOKEN)
+make_core_api_call("chemicalSimilaritySearch",
+                   :smiles => %(CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl),
+                   :chemspider_token => CHEMSPIDER_TOKEN)
+make_core_api_call("chemicalSubstructureSearch",
+                   :smiles => %(O=C(O)c2c(OCCN1C(=O)\\C=C/C1=O)cccc2),
+                   :chemspider_token => CHEMSPIDER_TOKEN)
+
+
+OPS::ChemSpiderClient.new(CHEMSPIDER_TOKEN).structure_search(%(O=C3C(/Oc1ccccc1)=C(/c2ccc(cc2)S(=O)(=O)C)CC3))
+OPS::ChemSpiderClient.new(CHEMSPIDER_TOKEN).similarity_search(%(CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl))
+OPS::ChemSpiderClient.new(CHEMSPIDER_TOKEN).substructure_search(%(O=C(O)c2c(OCCN1C(=O)\\C=C/C1=O)cccc2))
