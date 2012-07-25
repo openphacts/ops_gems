bea82bd7f6497ecc1b72aec3b734eff8b3149794
Thomas Marek <thomas.marek@biosolveit.de>
2012-07-25 12:12:10 +0100

    LinkedDataCacheClient: Generalize the result of compound_info and add provenance info

--- lib/ops/linked_data_cache_client.rb
+++ lib/ops/linked_data_cache_client.rb
@@ -49,15 +49,19 @@ module OPS
       unless primary_topics.children.empty?
         primary_topic = primary_topics.first
 
-        result = parse_property_nodes(primary_topic.xpath('./*[not(self::exactMatch) and not(self::inDataset)]'))
+        result = {
+          primary_topic.xpath('./inDataset').first['href'] => {
+            :href => primary_topic['href'],
+            :properties => parse_property_nodes(primary_topic.xpath('./*[not(self::exactMatch) and not(self::inDataset)]'))
+          }
+        }
 
         primary_topic.xpath("./exactMatch/item[@href != '#{primary_topic['href']}']").each do |item|
-          result.merge!(parse_property_nodes(item.xpath('./*[not(self::exactMatch) and not(self::inDataset)]')))
+          result[item.xpath('./inDataset').first['href']] = {
+            :href => item['href'],
+            :properties => parse_property_nodes(item.xpath('./*[not(self::exactMatch) and not(self::inDataset)]'))
+          }
         end
-
-        chemspider_uri = find_chemspider_uri(primary_topic)
-        result[:csid_uri] = chemspider_uri if chemspider_uri
-        result[:compound_name] = result.delete(:prefLabel) if result.has_key?(:prefLabel)
       end
 
       OPS.log(self, :info, "Result: #{result.nil? ? 0 : result.length} items")
@@ -74,14 +78,6 @@ module OPS
       end
 
       properties
-    end
-
-    def find_chemspider_uri(primary_topic)
-      if primary_topic.xpath("./inDataset[@href = 'http://www.chemspider.com']").empty?
-        primary_topic.xpath("./exactMatch/item/inDataset[@href = 'http://www.chemspider.com']").first.parent['href']
-      else
-        primary_topic['href']
-      end
     end
   end
 end
--- spec/linked_data_cache_client_spec.rb
+++ spec/linked_data_cache_client_spec.rb
@@ -24,30 +24,48 @@ describe OPS::LinkedDataCacheClient, :vcr do
       }.to raise_exception(ArgumentError)
     end
 
-    it "returns the compound info" do
+    it "returns the compound info if the compound is known to OPS" do
       @client.compound_info("http://rdf.chemspider.com/187440").should == {
-        :compound_name => "Sorafenib",
-        :csid_uri => "http://rdf.chemspider.com/187440",
-
-        :smiles => "CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl",
-        :inchikey => "MLDQJTXFUGDVEO-UHFFFAOYSA-N",
-        :inchi => "InChI=1S/C21H16ClF3N4O3/c1-26-19(30)18-11-15(8-9-27-18)32-14-5-2-12(3-6-14)28-20(31)29-13-4-7-17(22)16(10-13)21(23,24)25/h2-11H,1H3,(H,26,30)(H2,28,29,31)",
-        :rtb => "6",
-        :psa => "92.35",
-        :mw_freebase => "464.825",
-        :molform => "C21H16ClF3N4O3",
-        :hbd => "3",
-        :hba => "4",
-        :full_mwt => "464.825",
-        :alogp => "4.175",
-        :toxicity => "The highest dose of sorafenib studied clinically is 800 mg twice daily. The adverse reactions observed at this dose were primarily diarrhea and dermatologic events. No information is available on symptoms of acute overdose in animals because of the saturation of absorption in oral acute toxicity studies conducted in animals.",
-        :proteinBinding => "99.5%",
-        :description => "Sorafenib (rINN), marketed as Nexavar by Bayer, is a drug approved for the treatment of advanced renal cell carcinoma (primary kidney cancer). It has also received \"Fast Track\" designation by the FDA for the treatment of advanced hepatocellular carcinoma (primary liver cancer), and has since performed well in Phase III trials.\nSorafenib is a small molecular inhibitor of Raf kinase, PDGF (platelet-derived growth factor), VEGF receptor 2 & 3 kinases and c Kit the receptor for Stem cell factor. A growing number of drugs target most of these pathways. The originality of Sorafenib lays in its simultaneous targeting of the Raf/Mek/Erk pathway.",
-        :biotransformation => "Sorafenib is metabolized primarily in the liver, undergoing oxidative metabolism, mediated by CYP3A4, as well as glucuronidation mediated by UGT1A9. Sorafenib accounts for approximately 70-85% of the circulating analytes in plasma at steady- state. Eight metabolites of sorafenib have been identified, of which five have been detected in plasma. The main circulating metabolite of sorafenib in plasma, the pyridine N-oxide, shows <i>in vitro</i> potency similar to that of sorafenib. This metabolite comprises approximately 9-16% of circulating analytes at steady-state."
+        "http://www.chemspider.com" => {
+          :href => "http://rdf.chemspider.com/187440",
+          :properties => {
+            :smiles => "CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl",
+            :inchikey => "MLDQJTXFUGDVEO-UHFFFAOYSA-N",
+            :inchi => "InChI=1S/C21H16ClF3N4O3/c1-26-19(30)18-11-15(8-9-27-18)32-14-5-2-12(3-6-14)28-20(31)29-13-4-7-17(22)16(10-13)21(23,24)25/h2-11H,1H3,(H,26,30)(H2,28,29,31)"
+          }
+        },
+        "http://data.kasabi.com/dataset/chembl-rdf" => {
+          :href => "http://data.kasabi.com/dataset/chembl-rdf/molecule/m276734",
+          :properties => {
+            :rtb => "6",
+            :psa => "92.35",
+            :mw_freebase => "464.825",
+            :molform => "C21H16ClF3N4O3",
+            :hbd => "3",
+            :hba => "4",
+            :full_mwt => "464.825",
+            :alogp => "4.175"
+          }
+        },
+        "http://linkedlifedata.com/resource/drugbank" => {
+          :href => "http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugs/DB00398",
+          :properties => {
+            :toxicity => "The highest dose of sorafenib studied clinically is 800 mg twice daily. The adverse reactions observed at this dose were primarily diarrhea and dermatologic events. No information is available on symptoms of acute overdose in animals because of the saturation of absorption in oral acute toxicity studies conducted in animals.",
+            :proteinBinding => "99.5%",
+            :description => "Sorafenib (rINN), marketed as Nexavar by Bayer, is a drug approved for the treatment of advanced renal cell carcinoma (primary kidney cancer). It has also received \"Fast Track\" designation by the FDA for the treatment of advanced hepatocellular carcinoma (primary liver cancer), and has since performed well in Phase III trials.\nSorafenib is a small molecular inhibitor of Raf kinase, PDGF (platelet-derived growth factor), VEGF receptor 2 & 3 kinases and c Kit the receptor for Stem cell factor. A growing number of drugs target most of these pathways. The originality of Sorafenib lays in its simultaneous targeting of the Raf/Mek/Erk pathway.",
+            :biotransformation => "Sorafenib is metabolized primarily in the liver, undergoing oxidative metabolism, mediated by CYP3A4, as well as glucuronidation mediated by UGT1A9. Sorafenib accounts for approximately 70-85% of the circulating analytes in plasma at steady- state. Eight metabolites of sorafenib have been identified, of which five have been detected in plasma. The main circulating metabolite of sorafenib in plasma, the pyridine N-oxide, shows <i>in vitro</i> potency similar to that of sorafenib. This metabolite comprises approximately 9-16% of circulating analytes at steady-state."
+          }
+        },
+        "http://www.conceptwiki.org" => {
+          :href=>"http://www.conceptwiki.org/concept/38932552-111f-4a4e-a46a-4ed1d7bdf9d5",
+          :properties => {
+            :prefLabel => "Sorafenib"
+          }
+        }
       }
     end
 
-    it "returns the same result for different URIs of the same compound" do
+    it "returns the same result for different URIs of the same known compound" do
       conceptwiki_uri_result = @client.compound_info("http://www.conceptwiki.org/concept/38932552-111f-4a4e-a46a-4ed1d7bdf9d5")
       chemspider_uri_result = @client.compound_info("http://rdf.chemspider.com/187440")
 
