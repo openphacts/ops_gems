6bd1afb6a406c0c7ee80fc85c49816c824012aaf
Thomas Marek <thomas.marek@biosolveit.de>
2012-08-21 10:20:19 +0100

    Improve the example

--- example.rb
+++ example.rb
@@ -5,21 +5,34 @@ $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'ops'
 
 
-OPS_URL = "http://ops.few.vu.nl:9187/opsapi"
+OPS_LINKED_DATA_CACHE_URL = "http://ops2.few.vu.nl"
 CHEMSPIDER_TOKEN = ""
 
-
-raise "No OPS Url defined" if OPS_URL.empty?
+raise "No OPS Linked Data Cache Url defined" if OPS_LINKED_DATA_CACHE_URL.empty?
 raise "No ChemSpider Token defined" if CHEMSPIDER_TOKEN.empty?
 
 
 
+#OPS.log = false
 
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1))
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :all_tautomers)
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :same_skeleton_including_h)
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :same_skeleton_excluding_h)
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :all_isomers)
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).structure_search(%(O=C3C(/Oc1ccccc1)=C(/c2ccc(cc2)S(=O)(=O)C)CC3))
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).similarity_search(%(CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl))
-OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN).substructure_search(%(O=C(O)c2c(OCCN1C(=O)\\C=C/C1=O)cccc2))
+
+
+chemspider_client = OPS::ChemspiderClient.new(CHEMSPIDER_TOKEN)
+
+chemspider_client.structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1))
+chemspider_client.structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :all_tautomers)
+chemspider_client.structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :same_skeleton_including_h)
+chemspider_client.structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :same_skeleton_excluding_h)
+chemspider_client.structure_search(%([O-]C(=O)[C@@H](NC(=O)C[NH3+])Cc1ccc(O)cc1), :match_type => :all_isomers)
+# Note: Takes some time
+chemspider_client.substructure_search(%(O=C(O)c2c(OCCN1C(=O)\\C=C/C1=O)cccc2))
+# Note: Takes some time
+chemspider_client.similarity_search(%(CNC(=O)c1cc(ccn1)Oc2ccc(cc2)NC(=O)Nc3ccc(c(c3)C(F)(F)F)Cl))
+
+
+
+linked_data_cache_client = OPS::LinkedDataCacheClient.new(OPS_LINKED_DATA_CACHE_URL)
+
+linked_data_cache_client.compound_info("http://rdf.chemspider.com/187440")
+# Note: Takes some time
+linked_data_cache_client.compound_pharmacology_info("http://rdf.chemspider.com/187440")
