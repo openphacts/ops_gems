ac789f5df38f5c7c825164f4fc414d888d101719
Lothar Wissler <Lothar.Wissler@BioSolveIT.de>
2013-02-28 15:16:55 +0100

    * added drugbank URI mapping

--- lib/ops/uri_mapping.rb
+++ lib/ops/uri_mapping.rb
@@ -5,21 +5,21 @@
 #
 # This file is part of the OPS gem, made available under the MIT license.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of 
-# this software and associated documentation files (the "Software"), to deal in 
-# the Software without restriction, including without limitation the rights to use, 
-# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
-# Software, and to permit persons to whom the Software is furnished to do so, 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
 # subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all 
+# The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 # For further information please contact:
@@ -34,6 +34,7 @@ module OPS
     MAPPINGS = [
       [/http:\/\/rdf\.chemspider\.com\/(\d+)/, "http://www.chemspider.com/Chemical-Structure.%s.html"],
       [/http:\/\/www4\.wiwiss\.fu-berlin\.de\/drugbank\/resource\/drugs\/([\w\d]+)/, "http://www4.wiwiss.fu-berlin.de/drugbank/page/drugs/%s"],
+      [/http:\/\/wifo.*\.informatik\.uni-mannheim\.de\/drugbank\/page\/drugs\/(DB\d+)/, "http://www.drugbank.ca/drugs/%s"],
       [/http:\/\/data\.kasabi\.com\/dataset\/chembl-rdf\/chemblid\/(CHEMBL\d+)/, "https://www.ebi.ac.uk/chembldb/compound/inspect/%s"],
       [/http:\/\/data\.kasabi\.com\/dataset\/chembl-rdf\/(chemblid|molecule|target|activity|assay)\/([\w\d]+)/, "http://linkedchemistry.info/chembl/%s/%s"],
       [/http:\/\/www\.conceptwiki\.org\/concept\/([\w\d\-]+)/, "http://www.conceptwiki.org/wiki/#/concept/%s/view"],
